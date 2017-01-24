f8312d6975eaa16c022cf6aecd3c5f05f773a485
lamontu <lamontyu@163.com>
2017-01-24 17:41:57 +0800

    Represent directed graph

---
+++ graph/adjacency_list_dg.cpp
@@ -0,0 +1,241 @@
+/* adjacency_list_dg.cpp
+ * Adjacency List of Directed Graph
+ */
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 100;
+
+class ListDg {
+ private:
+  struct ENode {
+    int vertex_index;
+    ENode* next_edge;
+  };
+
+  struct VNode {
+    char data;
+    ENode* first_edge;
+  };
+
+  int vertex_num_;
+  int edge_num_;
+  VNode vertexes_[MAX];
+
+ public:
+  ListDg();
+  ListDg(char vertexes[], int vlen, char edges[][2], int elen);
+  ~ListDg();
+  void DFS() const;
+  void BFS() const;
+  void Print() const;
+
+ private:
+  char read_char();
+  int get_position(char ch) const;
+  void link_last(ENode* list, ENode* node) const;
+  void dfs(int i, int* visited) const;
+};
+
+char ListDg::read_char() {
+  char ch;
+  do {
+    cin >> ch;
+  } while (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')));
+  return ch;
+}
+
+int ListDg::get_position(char ch) const {
+  int i;
+  for (i = 0; i < vertex_num_; ++i) {
+    if(vertexes_[i].data == ch) return i;
+  }
+  return -1;
+}
+
+void ListDg::link_last(ENode* list, ENode* node) const {
+  ENode* p = list;
+  while (p->next_edge) {
+    p = p->next_edge;
+  }
+  p->next_edge = node;
+}
+
+ListDg::ListDg() {
+  char c1, c2;
+  int v, e;
+  int i, p1, p2;
+  ENode *node1, *node2;
+  
+  cout << "Input vertex number:";
+  cin >> vertex_num_;
+  cout << "Input edge number:";
+  cin >> edge_num_;
+  if (vertex_num_ < 1 || edge_num_ < 0
+      || (edge_num_ > (vertex_num_ * (vertex_num_ - 1)))) {
+    cout << "Input error: invalid parameters!" << endl;
+    return;
+  }
+  for (i = 0; i < vertex_num_; ++i) {
+    cout << "vertex(" << i <<"): ";
+    vertexes_[i].data = read_char();
+    vertexes_[i].first_edge = nullptr;
+  }
+  for (i = 0; i < edge_num_; ++i) {
+    cout << "edge(" << i << "): ";
+    c1 = read_char();
+    c2 = read_char();
+    p1 = get_position(c1);
+    p2 = get_position(c2);
+    node1 = new ENode();
+    node1->vertex_index = p2;
+    if (nullptr == vertexes_[p1].first_edge) {
+      vertexes_[p1].first_edge = node1;
+    } else {
+      link_last(vertexes_[p1].first_edge, node1);
+    }
+
+/* This is different from directed graph.
+    node2 = new ENode();
+    node2->vertex_index = p1;
+    if (nullptr == vertexes_[p2].first_edge) {
+      vertexes_[p2].first_edge = node2;
+    } else {
+      link_last(vertexes_[p2].first_edge, node2);
+    }
+ */
+  }
+}
+
+ListDg::ListDg(char vertexes[], int vlen, char edges[][2], int elen) {
+  char c1, c2;
+  int i, p1, p2;
+  ENode *node1, *node2;
+  vertex_num_ = vlen;
+  edge_num_ = elen;
+  for (i = 0; i < vertex_num_; ++i) {
+    vertexes_[i].data = vertexes[i];
+    vertexes_[i].first_edge = nullptr;
+  }
+  for (i = 0; i < edge_num_; ++i) {
+    c1 = edges[i][0];
+    c2 = edges[i][1];
+    p1 = get_position(c1);
+    p2 = get_position(c2);
+    node1 = new ENode();
+    node1->vertex_index = p2;
+    if (nullptr == vertexes_[p1].first_edge) {
+      vertexes_[p1].first_edge = node1;
+    } else {
+      link_last(vertexes_[p1].first_edge, node1);
+    }
+
+/* This is different from directed graph.
+    node2 = new ENode();
+    node2->vertex_index = p1;
+    if (nullptr == vertexes_[p2].first_edge) {
+      vertexes_[p2].first_edge = node2;
+    } else {
+      link_last(vertexes_[p2].first_edge, node2);
+    }
+ */
+  }
+}
+
+void ListDg::dfs(int i, int* visited) const {
+  ENode* node;
+  visited[i] = 1;
+  cout << vertexes_[i].data <<", ";
+  node = vertexes_[i].first_edge;
+  while (node != nullptr) {
+    if (!visited[node->vertex_index]) {
+      dfs(node->vertex_index, visited);
+    }
+    node = node->next_edge;
+  }
+}
+
+void ListDg::DFS() const {
+  int i;
+  int visited[MAX];
+  for (i = 0; i < vertex_num_; ++i) {
+    visited[i] = 0;
+  }
+  cout << "DFS: ";
+  for (i = 0; i < vertex_num_; ++i) {
+    if (!visited[i]) {
+      dfs(i, visited);
+    }
+  }
+  cout << endl;
+}
+
+void ListDg::BFS() const {
+  int head = 0;
+  int rear = 0;
+  int queue[MAX];
+  int visited[MAX];
+  int i, j, k;
+  ENode* node;
+  for (i = 0; i < vertex_num_; ++i) {
+    visited[i] = 0;
+  }
+  cout << "BFS: ";
+  for (i = 0; i < vertex_num_; ++i) {
+    if (!visited[i]) {
+      visited[i] = 1;
+      cout << vertexes_[i].data << ", ";
+      queue[rear++] = i;
+    }
+    while (head != rear) {
+      j = queue[head++];
+      node = vertexes_[j].first_edge;
+      while (node != nullptr) {
+        k = node->vertex_index;
+        if (!visited[k]) {
+          visited[k] = 1;
+          cout << vertexes_[k].data << ", ";
+          queue[rear++] = k;
+        }
+        node = node->next_edge;
+      }
+    }
+  }
+  cout << endl;
+}
+
+void ListDg::Print() const {
+  int i, j;
+  ENode* node;
+  cout << "List Graph:" << endl;
+  for (i = 0; i < vertex_num_; ++i) {
+    cout << i << "(" << vertexes_[i].data << "): ";
+    node = vertexes_[i].first_edge;
+    while (node != nullptr) {
+      cout << node->vertex_index << "("
+           << vertexes_[node->vertex_index].data << ")";
+      node = node->next_edge;
+    }
+    cout << endl;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  char vertexes[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+  char edges[][2] = {
+    {'A', 'C'}, {'A', 'D'}, {'A', 'F'}, {'B', 'C'},
+    {'C', 'D'}, {'E', 'G'}, {'F', 'G'}
+  };
+  int vlen = sizeof(vertexes) / sizeof(vertexes[0]);
+  int elen = sizeof(edges) / sizeof(edges[0]);
+  ListDg* pDg;
+  // pDg = new ListDg();  // Manually input
+  pDg = new ListDg(vertexes, vlen, edges, elen);
+  pDg->DFS();
+  pDg->BFS();
+  pDg->Print();
+
+  return 0;
+}
---
+++ graph/adjacency_matrix_dg.cpp
@@ -0,0 +1,200 @@
+/* adjacency_matrix_dg.cpp
+ * Adjacency Matrix of Directed Graph
+ */
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 100;
+
+class MatrixDg {
+ private:
+  char vertexes_[MAX];
+  int vertex_num_;
+  int edge_num_;
+  int matrix_[MAX][MAX];
+
+ public:
+  MatrixDg();
+  MatrixDg(char vertexes[], int vlen, char edges[][2], int elen);
+  ~MatrixDg() {  }
+  void DFS() const;
+  void BFS() const;
+  void Print() const;
+
+ private:
+  char read_char();
+  int get_position(char ch) const;
+  int first_vertex(int v) const;
+  int next_vertex(int v, int w) const;
+  void dfs(int i, int* visited) const;
+};
+
+char MatrixDg::read_char() {
+  char ch;
+  // Input one character one time
+  do {
+    cin >> ch;
+  } while (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')));
+  return ch;
+}
+
+int MatrixDg::get_position(char ch) const {
+  int i;
+  for (i = 0; i < vertex_num_; ++i) {
+    if(vertexes_[i] == ch) return i;
+  }
+  return -1;
+}
+
+MatrixDg::MatrixDg() {
+  char c1, c2;
+  int i, p1, p2;
+  cout << "Input vertex number:";
+  cin >> vertex_num_;
+  cout << "Input edge number:";
+  cin >> edge_num_;
+  if (vertex_num_ < 1 || edge_num_ < 0
+      || (edge_num_ > (vertex_num_ * (vertex_num_ - 1)))) {
+    cout << "Input error: invalid parameters!" << endl;
+    return;
+  }
+  for (i = 0; i < vertex_num_; ++i) {
+    cout << "vertex(" << i << "): ";
+    vertexes_[i] = read_char();
+  }
+  for (i = 0; i < edge_num_; ++i) {
+    cout << "edge(" << i << "): ";
+    c1 = read_char();
+    c2 = read_char();
+    p1 = get_position(c1);
+    p2 = get_position(c2);
+    if (p1 == -1 || p2 == -1) {
+      cout << "Input error: invalid edge!" << endl;
+      return;
+    }
+    matrix_[p1][p2] = 1;
+    // matrix_[p2][p1] = 1;  // This is different from directed graph.
+  }
+}
+
+MatrixDg::MatrixDg(char vertexes[], int vlen, char edges[][2], int elen) {
+  int i, p1, p2;
+  vertex_num_ = vlen;
+  edge_num_ = elen;
+  for (i = 0; i < vertex_num_; ++i) {
+    vertexes_[i] = vertexes[i];
+  }
+  for (i = 0; i < edge_num_; ++i) {
+    p1 = get_position(edges[i][0]);
+    p2 = get_position(edges[i][1]);
+    matrix_[p1][p2] = 1;
+    // matrix_[p2][p1] = 1;  // This is different from directed graph.
+  }
+}
+
+int MatrixDg::first_vertex(int v) const {
+  int i;
+  if (v < 0 || v > (vertex_num_ - 1)) return -1;
+  for (i = 0; i < vertex_num_; ++i) {
+    if (1 == matrix_[v][i]) return i;
+  }
+  return -1;
+}
+
+int MatrixDg::next_vertex(int v, int w) const {
+  int i;
+  if (v < 0 || v > (vertex_num_ - 1) || w < 0 || w > (vertex_num_ - 1)) {
+    return -1;
+  }
+  for (i = w + 1; i < vertex_num_; ++i) {
+    if (1 == matrix_[v][i]) return i;
+  }
+  return -1;
+}
+
+void MatrixDg::dfs(int i, int* visited) const {
+  int w;
+  visited[i] = 1;
+  cout << vertexes_[i] << ", ";
+  for (w = first_vertex(i); w >= 0; w = next_vertex(i, w)) {
+    if (!visited[w]) {
+      dfs(w, visited);
+    }
+  }
+}
+
+void MatrixDg::DFS() const {
+  int i;
+  int visited[MAX];
+  for (i = 0; i < vertex_num_; ++i) {
+    visited[i] = 0;
+  }
+  cout << "DFS: ";
+  for (i = 0; i < vertex_num_; ++i) {
+    if (!visited[i]) {
+      dfs(i, visited);
+    }
+  }
+  cout << endl;
+}
+
+void MatrixDg::BFS() const {
+  int head = 0;
+  int rear = 0;
+  int queue[MAX];
+  int visited[MAX];
+  int i, j, k;
+  for (i = 0; i < vertex_num_; ++i) {
+    visited[i] = 0;
+  }
+  cout << "BFS: ";
+  for (i = 0; i < vertex_num_; ++i) {
+    if (!visited[i]) {
+      visited[i] = 1;
+      cout << vertexes_[i] << ", ";
+      queue[rear++] = i;
+    }
+    while (head != rear) {
+      j = queue[head++];
+      for (k = first_vertex(j); k >= 0; k = next_vertex(j, k)) {
+        if (!visited[k]) {
+          visited[k] = 1;
+          cout << vertexes_[k] << ", ";
+          queue[rear++] = k;
+        }
+      }
+    }
+  }
+  cout << endl;
+} 
+
+void MatrixDg::Print() const {
+  int i, j;
+  cout << "Matrix Graph:" << endl;
+  for (i = 0; i < vertex_num_; ++i) {
+    for (j = 0; j < vertex_num_; ++j) {
+      cout << matrix_[i][j] << ", ";
+    }
+    cout << endl;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  char vertexes[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+  char edges[][2] = {
+    {'A', 'C'}, {'A', 'D'}, {'A', 'F'}, {'B', 'C'},
+    {'C', 'D'}, {'E', 'G'}, {'F', 'G'}
+  };
+  int vlen = sizeof(vertexes) / sizeof(vertexes[0]);
+  int elen = sizeof(edges) / sizeof(edges[0]);
+  MatrixDg* pDg;
+  // pDg = new MatrixDg();  // Manually input
+  pDg = new MatrixDg(vertexes, vlen, edges, elen);
+  pDg->DFS();
+  pDg->BFS();
+  pDg->Print();
+
+  return 0;
+}
