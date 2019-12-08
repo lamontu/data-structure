b09e61750f973eb0a78ad68e2936a3eb7ec83272
lamontu <lamontyu@163.com>
2019-12-08 11:18:48 +0800

    Added copy constructor and assignment operator

--- queue_use_array.cpp
+++ queue_use_array.cpp
@@ -5,19 +5,51 @@
 
 using namespace std;
 
- 
 class Queue {
  private:
   int* data;
   int head, tail, length, count;
 
  public:
-  Queue(int length_input) {
+  explicit Queue(int length_input) {
     data = new int[length_input];
     length = length_input;
     head = 0;
     tail = -1;
     count = 0;
+  }
+
+  Queue(const Queue& other) {
+    head = other.head;
+    tail = other.tail;
+    length = other.length;
+    count = other.count;
+    data = new int[length];
+    for (int i = head, c = 0;
+         i != tail + 1 && c < count;
+         i = (i + 1) % length, ++c) {
+      data[i] = other.data[i];
+    }
+  }
+
+  Queue& operator=(const Queue& rhs){
+    if (this == &rhs) {
+      return *this;
+    }
+
+    delete[] data;
+
+    head = rhs.head;
+    tail = rhs.tail;
+    length = rhs.length;
+    count = rhs.count;
+    data = new int[length];
+    for (int i = head, c = 0;
+         i != tail + 1 && c < count;
+         i = (i + 1) % length, ++c) {
+      data[i] = rhs.data[i];
+    }
+    return *this;
   }
 
   ~Queue() {
@@ -68,8 +100,7 @@ class Queue {
   }
 };
 
-
-int main(int argc, char* argv[]) {
+int main() {
   Queue queue(10);
   for (int i = 1; i <= 10; ++i) {
     queue.push(i);
@@ -79,5 +110,3 @@ int main(int argc, char* argv[]) {
   queue.output();
   return 0;
 }
-
-
