969a72f3e2ea738211b1b0f46538bd19643e83b5
lamontu <lamontyu@163.com>
2020-01-29 15:34:32 +0800

    Removed warnings

--- graph/adjacency_matrix_dg.cpp
+++ graph/adjacency_matrix_dg.cpp
@@ -10,10 +10,10 @@ const int MAX = 100;
 
 class MatrixDg {
  private:
-  char vertexes_[MAX];
   size_t vertex_num_;
   size_t edge_num_;
   int matrix_[MAX][MAX];
+  char vertexes_[MAX];
 
  public:
   MatrixDg();
@@ -27,9 +27,9 @@ class MatrixDg {
  private:
   char read_char();
   size_t get_position(char ch) const;
-  int first_vertex(int v) const;
-  int next_vertex(int v, int w) const;
-  void dfs(int i, bool* visited) const;
+  size_t first_vertex(size_t v) const;
+  size_t next_vertex(size_t v, size_t w) const;
+  void dfs(size_t i, bool* visited) const;
 
 };
 
@@ -91,37 +91,38 @@ MatrixDg::MatrixDg(char vertexes[], size_t vlen, char edges[][2], size_t elen) {
   for (i = 0; i < edge_num_; ++i) {
     size_t p1 = get_position(edges[i][0]);
     size_t p2 = get_position(edges[i][1]);
+    if (p1 == MAX || p2 == MAX) {
+      cout << "Input error: invalid edge!" << endl;
+      return;
+    }
     matrix_[p1][p2] = 1;
     // matrix_[p2][p1] = 1;  // This is different from undirected graph.
   }
 
 }
 
-int MatrixDg::first_vertex(int v) const {
-  int i;
-  if (v < 0 || v > (vertex_num_ - 1)) return -1;
-  for (i = 0; i < vertex_num_; ++i) {
+size_t MatrixDg::first_vertex(size_t v) const {
+  if (v > vertex_num_ - 1) return MAX;
+  for (size_t i = 0; i < vertex_num_; ++i) {
     if (1 == matrix_[v][i]) return i;
   }
-  return -1;
+  return MAX;
 }
 
-int MatrixDg::next_vertex(int v, int w) const {
-  int i;
-  if (v < 0 || v > (vertex_num_ - 1) || w < 0 || w > (vertex_num_ - 1)) {
-    return -1;
+size_t MatrixDg::next_vertex(size_t v, size_t w) const {
+  if (v > vertex_num_ - 1 || w > vertex_num_ - 1) {
+    return MAX;
   }
-  for (i = w + 1; i < vertex_num_; ++i) {
+  for (size_t i = w + 1; i < vertex_num_; ++i) {
     if (1 == matrix_[v][i]) return i;
   }
-  return -1;
+  return MAX;
 }
 
-void MatrixDg::dfs(int i, bool* visited) const {
-  int w;
+void MatrixDg::dfs(size_t i, bool* visited) const {
   visited[i] = true;
   cout << vertexes_[i] << ", ";
-  for (w = first_vertex(i); w >= 0; w = next_vertex(i, w)) {
+  for (size_t w = first_vertex(i); w != MAX; w = next_vertex(i, w)) {
     if (!visited[w]) {
       dfs(w, visited);
     }
@@ -161,8 +162,7 @@ void MatrixDg::BFS() const {
     }
     while (head != rear) {
       j = queue[head++];
-      int k;
-      for (k = first_vertex(j); k >= 0; k = next_vertex(j, k)) {
+      for (size_t k = first_vertex(j); k != MAX; k = next_vertex(j, k)) {
         if (!visited[k]) {
           visited[k] = true;
           cout << vertexes_[k] << ", ";
